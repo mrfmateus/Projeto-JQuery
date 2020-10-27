6865d346e3365add9cec8a1d1eb11836a28ad401
Mateus Ferreira <mrf.mateus@hotmail.com>
2020-10-27 13:00:42 -0300

    Temporizador e contador de caracteres/palavras

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,25 @@
 var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
+var numPalavras = frase.split(/\S+/).length - 1;
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
+
+var inputDigitacao = $("#digitacao");
+inputDigitacao.on("input", function () {
+  qtdCaracteres = this.value.length;
+  qtdPalavras = this.value.split(/\S+/).length - 1;
+  $("#contador-caracteres").text(qtdCaracteres);
+  $("#contador-palavras").text(qtdPalavras);
+});
+
+var campoTempo = $("#tempo-restante");
+inputDigitacao.one("focus", function () {
+  var tempoRestante = campoTempo.text();
+  var tempoIntervalo = setInterval(function () {
+    tempoRestante--;
+    campoTempo.text(tempoRestante);
+    if (tempoRestante <= 0) {
+      inputDigitacao.attr("disabled", true);
+      clearInterval(tempoIntervalo);
+    }
+  }, 1000);
+});
