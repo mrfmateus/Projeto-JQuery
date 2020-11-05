ae22d17dbb2284f4e02631555351470d9d3aca03
Mateus Ferreira <mrf.mateus@hotmail.com>
2020-11-05 13:06:40 -0300

    (Início da pt2) Implementando animação com Jquery

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,3 +1,7 @@
+var secPlacar = $(".placar");
+var botaoPlacar = $("#botao-placar");
+botaoPlacar.click(mostraPlacar);
+
 function inserePlacarNaTabela() {
   var corpoTabela = $(".placar").find("tbody");
   var usuario = "Mateus";
@@ -5,6 +9,8 @@ function inserePlacarNaTabela() {
   var linhaNova = criaLinhaNova(usuario, qtdPalavras);
   linhaNova.find(".botao-remover").click(removePlacarDaTabela);
   corpoTabela.prepend(linhaNova);
+  secPlacar.slideDown(500);
+  scrollPlacar();
 }
 
 function criaLinhaNova(usuario, palavras) {
@@ -26,5 +32,20 @@ function criaLinhaNova(usuario, palavras) {
 
 function removePlacarDaTabela() {
   event.preventDefault();
-  $(this).parent().parent().remove();
+  var linha = $(this).parent().parent();
+  linha.fadeOut(700);
+  setTimeout(function () {
+    linha.remove();
+  }, 700);
+}
+
+function mostraPlacar(event) {
+  secPlacar.stop().slideToggle(500);
+}
+
+function scrollPlacar() {
+  var posicaoPlacar = secPlacar.offset().top;
+  $("html, body").animate({
+    scrollTop: posicaoPlacar + "px"
+  }, 1000);
 }
