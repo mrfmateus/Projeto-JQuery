9d1a56425653e98593fba906f85f16dc35972a82
Mateus Ferreira <mrf.mateus@hotmail.com>
2020-11-18 13:00:24 -0300

    Adicionando gif de loading; Adicionando botão para buscar frase específica

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,18 +1,50 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-especifica").click(fraseEspecifica);
 
 function fraseAleatoria() {
-  $.get("http://localhost:3000/frases", trocaFrase).fail(function () {
-    $("#erro").toggle();
-    setTimeout(function () {
+  $("#spinner").toggle();
+  $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function () {
       $("#erro").toggle();
-    }, 2000);
-  });
+      setTimeout(function () {
+        $("#erro").toggle();
+      }, 2000);
+    })
+    .always(function () {
+      $("#spinner").toggle();
+    });
 }
 
-function trocaFrase(data) {
+function trocaFraseAleatoria(data) {
   var numeroAleatorio = Math.floor(Math.random() * data.length);
   var frase = $(".frase");
   frase.text(data[numeroAleatorio].texto);
   alteraTamanhoFrase();
   alteraTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function fraseEspecifica() {
+  var idFrase = $("#input-frase-especifica").val();
+  var dados = {
+    id: idFrase
+  };
+
+  $("#spinner").toggle();
+  $.get("http://localhost:3000/frases", dados, trocaFraseEspecifica)
+    .fail(function () {
+      $("#erro").toggle();
+      setTimeout(function () {
+        $("#erro").toggle();
+      }, 2000);
+    })
+    .always(function () {
+      $("#spinner").toggle();
+    });
+}
+
+function trocaFraseEspecifica(data) {
+  var frase = $(".frase");
+  frase.text(data.texto);
+  alteraTamanhoFrase();
+  alteraTempoInicial(data.tempo);
 }
